cb7f6805ab9bcd6ce6cb4ee8b2f4273afde9f429
cruiserkasuga <jason.palomino@sjsu.edu>
2025-10-20 22:46:47 -0700

    login request and message response created

--- backend/src/main/java/com/lostfound/backend/controller/AuthController.java
+++ backend/src/main/java/com/lostfound/backend/controller/AuthController.java
@@ -1,4 +1,35 @@
 package com.lostfound.backend.controller;
 
+import com.lostfound.backend.repositories.UserRepository;
+import com.lostfound.backend.security.request.SignupRequest;
+import com.lostfound.backend.security.response.MessageResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+        if (userRepository.existsByUsername(signupRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+        }
+        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already taken!"));
+        }
+        if (userRepository.existsByPhoneNumber(signupRequest.getPhoneNumber())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Phone number is already taken!"));
+        }
+
+        return null;
+    }
 }
--- backend/src/main/java/com/lostfound/backend/security/WebSecurityConfig.java
+++ backend/src/main/java/com/lostfound/backend/security/WebSecurityConfig.java
@@ -1,4 +1,9 @@
 package com.lostfound.backend.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 }
