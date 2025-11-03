90eb43a01b4d92bf84acc3dc798505bbf13f4080
Tonny Lee <tonny.lee@sjsu.edu>
2025-11-02 21:25:59 -0800

    added some attributes in postRequestDTO.java

--- backend/src/main/java/com/lostfound/backend/dto/PostRequestDTO.java
+++ backend/src/main/java/com/lostfound/backend/dto/PostRequestDTO.java
@@ -1,4 +1,14 @@
 package com.lostfound.backend.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PostRequestDTO {
+        private String title;
+        private String description;
+        private String location;
+        private String type;
+        private int userId;
 }
