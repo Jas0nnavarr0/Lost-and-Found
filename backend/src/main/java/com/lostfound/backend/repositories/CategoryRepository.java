78f395b95838d2b7bb8e85d4b0ffe9c1bc82d695
Tonny Lee <tonny.lee@sjsu.edu>
2025-11-04 20:00:40 -0800

    created category service

--- backend/src/main/java/com/lostfound/backend/repositories/CategoryRepository.java
+++ backend/src/main/java/com/lostfound/backend/repositories/CategoryRepository.java
@@ -4,7 +4,15 @@ import com.lostfound.backend.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository <Category, Integer> {
-    
+    // JPA query method
+    // automatically generate a query
+    // no to write need sql myself
+    // matches the field name is the category entity
+    // containing -> means LIKE %Something% in the SQL
+    // case-insensitive
+    List<Category> findByNameContainingIgnoreCase(String name);
 }
