99d06e062e9abd602bbef5e82e95fb76dc0c5175
Tonny Lee <tonny.lee@sjsu.edu>
2025-11-12 16:39:55 -0800

    add categories in DataLoader.java

--- backend/src/main/java/com/lostfound/backend/config/DataLoader.java
+++ backend/src/main/java/com/lostfound/backend/config/DataLoader.java
@@ -21,7 +21,23 @@ public class DataLoader implements CommandLineRunner {
     // This is the method that executes right after Spring Boot starts.
     @Override
     public void run(String[] args) {
-        List<String> defaultCategories = List.of("Electronics", "Documents", "Pets", "Jewelry", "Bags", "Phone", "Electronics");
+        List<String> defaultCategories = List.of(
+                "Electronics",
+                "Personal Items",
+                "Bags",
+                "Clothing",
+                "Documents",
+                "Jewelry",
+                "Money / Cards",
+                "Pets",
+                "Accessories",
+                "Wallet",
+                "Key",
+                "Books",
+                "School Items",
+                "Others"
+        );
+
 
         // check if the new category exits in the DB already.
         for(String category : defaultCategories){
