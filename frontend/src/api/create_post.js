f09a70d31bf65c2b67ac6af0b99cc21041692e93
Tonny <tonny.lee@sjsu.edu>
2025-11-27 03:26:39 -0800

    added credential

--- frontend/src/api/create_post.js
+++ frontend/src/api/create_post.js
@@ -3,11 +3,9 @@ import axios from "axios";
 const API_URL = "http://localhost:5000/api/posts";
 
 export const createPost = (postData) => {
-  const token = localStorage.getItem("token");  // <--- IMPORTANT
-
   return axios.post(API_URL, postData, {
+    withCredentials: true,  
     headers: {
-      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json"
     }
   });
